0f5775c2ed129e4e8fd90d3678bc6047755c0620
chang1111 <dream134@naver.com>
2017-09-22 12:59:11 +0900

    a

--- JAVA/Work/JavaApplication/src/main/java/java03/ex03_10_LowerUpper.java
+++ JAVA/Work/JavaApplication/src/main/java/java03/ex03_10_LowerUpper.java
@@ -9,13 +9,13 @@ public class ex03_10_LowerUpper {
         Scanner keyboard = new Scanner(System.in);
         
         System.out.print("ch : ");
-        char ch = (keyboard.next().charAt(0));
+        char ch = keyboard.next().charAt(0);
 
-        if (ch >= 65 && ch <= 90) {
+        if (ch >= 'A' && ch <= 'Z') {
             ch = (char)(ch + 32);
             System.out.print("ch to lowerCase : ");
         }
-        else if (ch >= 97 && ch <= 122) {
+        else if (ch >= 'a' && ch <= 'z') {
             ch = (char)(ch - 32);
             System.out.print("ch to upperCase : ");
         }
--- JAVA/Work/JavaApplication/src/main/java/java04/ex04_09.java
+++ JAVA/Work/JavaApplication/src/main/java/java04/ex04_09.java
@@ -1,22 +1,17 @@
 package java04;
 
-import java.util.Scanner;
-
 public class ex04_09 {
     
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        int n = keyboard.nextInt();
-        keyboard.close();
-        int sum = 0, sum0 = 0;
-        for (int i = 1;i <= n;i = i + 1) {
-            sum0 = 0;
+        int sum = 0;
+        for (int i = 1;i <= 10;i = i + 1) {
+            int sum0 = 0;
             for (int j = 1;j <= i; j = j + 1) {
                 sum0 = sum0 + j;
             }
             sum = sum + sum0;
         }
-        System.out.println(sum);
+        System.out.println("결과값은 : " + sum);
         
     }
     
