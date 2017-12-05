21ec915f43d9c03437e2bbcee88d130b388c1d2a
chang1111 <dream134@naver.com>
2017-12-05 18:10:46 +0900

    a

---
+++ Android/Work/part1.widget/ad01-04.FourButton/app/src/main/java/com/android/fourbutton/MainActivity.java
@@ -0,0 +1,63 @@
+package com.android.fourbutton;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1 = null;
+    private Button button2 = null;
+    private Button button3 = null;
+    private Button button4 = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nate.com"));
+                startActivity(i);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("tel://119"));
+                startActivity(i);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent i = new Intent(Intent.ACTION_PICK);
+//                i.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+//                i.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                String url = "content://media/internal/images/media";
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
