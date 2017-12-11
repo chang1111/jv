b746c568c9ee0e2b7347b768990eac6e8a30640b
chang1111 <dream134@naver.com>
2017-12-11 18:11:56 +0900

    a

---
+++ Android/Work/part1.widget/ad03-01.LayoutExample/st3relativelayout/src/main/java/com/android/st3relativelayout/Relative3Activity.java
@@ -0,0 +1,13 @@
+package com.android.st3relativelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Relative3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_relative3);
+    }
+}
